a24e6d43a4c8eb4b980f544afcf7574b3d4f0ca2
Abdimalik <binabdimalikranger@gmail.com>
2026-01-18 11:31:48 +0300

    debugged

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,4 +4,28 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/services': {
+        target: 'http://127.0.0.1:5555',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/categories': {
+        target: 'http://127.0.0.1:5555',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/bookings': {
+        target: 'http://127.0.0.1:5555',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/users': {
+        target: 'http://127.0.0.1:5555',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 })
